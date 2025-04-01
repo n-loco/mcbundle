401c3e2a0237f1eff844000ff7ec8830872193c4
Nícolas N_Loco R. Santos <nicolasrsantos.nloco@proton.me>
2025-03-31 22:38:25 -0300

    .

--- commojang/commojang_nowin32.go
+++ commojang/commojang_nowin32.go
@@ -12,8 +12,8 @@ func WarnComMojangPath(should bool) {
 	if should {
 		_, exists := os.LookupEnv(ComMojangPathVarKey)
 		if !exists {
-			terminal.Wprint("deducing the path to com.mojang is not possible;\n")
-			terminal.Wprintf("consider adding %s to your environment variables.\n", ComMojangPathVarKey)
+			terminal.Wprint("deducing the path to com.mojang is not possible\n")
+			terminal.Wprintf("consider adding %s to your environment variables\n", ComMojangPathVarKey)
 		}
 	}
 }
@@ -21,8 +21,8 @@ func WarnComMojangPath(should bool) {
 func ComMojangPath() string {
 	value, exists := os.LookupEnv(ComMojangPathVarKey)
 	if !exists {
-		terminal.Eprint("path to com.mojang directory not set;\n")
-		terminal.Eprintf("please add %s to your environment variables.\n", ComMojangPathVarKey)
+		terminal.Eprint("path to com.mojang directory not set\n")
+		terminal.Eprintf("please add %s to your environment variables\n", ComMojangPathVarKey)
 		os.Exit(1)
 	}
 	return value
