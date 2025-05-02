378a3f04ec7343703827838fa3a5b37ad8b6da4f
Nícolas N_Loco R. Santos <nicolasrsantos.nloco@proton.me>
2025-05-02 01:45:46 -0300

    build: configure npm/bpbuild/esbuild.js to bundle

--- npm/bpbuild/esbuild.js
+++ npm/bpbuild/esbuild.js
@@ -1,8 +1,12 @@
 import esbuild from "esbuild";
 
 esbuild.buildSync({
+    bundle: true,
+    packages: "external",
+    platform: "node",
+    format: "esm",
     entryPoints: ["./source/main.ts"],
     outfile: "./dist/bpbuild.mjs",
+    target: "node22",
     minifyWhitespace: true,
-    target: "node22"
 });
